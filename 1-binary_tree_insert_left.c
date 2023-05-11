0b63a01a99fede276db65f6e846d49c2cc213a17
jGohan-cpu <jjoseriveram01@gmail.com>
2023-05-11 20:30:08 +0000

    betty fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,32 +8,32 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node, *old_left_child;
+	binary_tree_t *new_node, *old_left_child;
 
-    if (parent == NULL)
-        return (NULL);
+	if (parent == NULL)
+	return (NULL);
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	return (NULL);
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->right = NULL;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->right = NULL;
 
-    if (parent->left != NULL)
-    {
-        old_left_child = parent->left;
-        parent->left = new_node;
-        new_node->left = old_left_child;
-        old_left_child->parent = new_node;
-    }
-    else
-    {
-        parent->left = new_node;
-        new_node->left = NULL;
-    }
+	if (parent->left != NULL)
+	{
+	old_left_child = parent->left;
+	parent->left = new_node;
+	new_node->left = old_left_child;
+	old_left_child->parent = new_node;
+	}
+	else
+	{
+	parent->left = new_node;
+	new_node->left = NULL;
+	}
 
-    return (new_node);
+	return (new_node);
 }
 
