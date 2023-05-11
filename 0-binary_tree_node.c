862c112bdfb3958511755ca96486da61dbca41fa
jGohan-cpu <jjoseriveram01@gmail.com>
2023-05-11 19:16:30 +0000

    betty added fixed

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,16 +1,17 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ *binary_tree_node - creates a binary tree node
+ *@parent: parent node
+ *@value: value within node
+ *Return: the binary tree node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
 	if (new_node == NULL)
-	return NULL;
+	return (NULL);
 
 	new_node->n = value;
 	new_node->parent = parent;
