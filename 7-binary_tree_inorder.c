5feb4855db182c38de7e1d28183cb434e0737157
jGohan-cpu <jjoseriveram01@gmail.com>
2023-05-12 18:54:02 +0000

    7th exercise

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 /**
  *binary_tree_inorder - tree using in-order traversal
- *@tree: tree used 
+ *@tree: tree used
  *@func: function pointed to
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL) 
+	if (tree == NULL || func == NULL)
 	{
 	return;
 	}
