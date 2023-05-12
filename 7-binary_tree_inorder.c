9f4e0381659b7770eb7cbd2b60c0ada8bc77be27
jGohan-cpu <jjoseriveram01@gmail.com>
2023-05-12 18:50:15 +0000

    6th exercise used

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,1 +1,11 @@
-#include "binary_trees.h>
+#include "binary_trees.h"
+/**
+ *
+ *
+ *
+ *
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+}
---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include <stddef.h> // for NULL
+#include <stdlib.h> // for size_t
+#include <stdio.h> // for debugging purposes
+#include "binary_trees.h"
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: The height of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+	return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (left_height > right_height ? left_height : right_height) + 1;
+}
+
